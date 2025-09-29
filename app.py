986bffaf36ca3cbeeec1d7f0b4013c3f6de106e6
Olliek123 <ollyking123@live.co.uk>
2025-09-29 16:39:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '''
-        <h1>Hello from Azure Web App!</h1>
-        <p>/aboutGo to About Page</a></p>
-    '''
+    return "Hello from Azure Web App!"
 
 @app.route('/about')
 def about():
-    return '''
-        <h1>About Page</h1>
-        <p>This is some information about the site.</p>
-        <p>/Back to Home</a></p>
-    '''
+    return "test"
 
 if __name__ == '__main__':
     app.run()
