6011895c2511b254a8f865c6c19844ddd93c67f5
Olliek123 <ollyking123@live.co.uk>
2025-09-29 16:08:50 +0100

    Update app.py
    added button and another page

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template_string
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Oliver's Web App, it's FIREEEE!"
+    return '''
+        <h1>Hello and welcome to my website!</h1>
+        /about<button>Go to About Page</button></a>
+    '''
+
+@app.route('/about')
+def about():
+    return '''
+        <h1>About Page</h1>
+        <p>This is a second page with some extra info.</p>
+        /<button>Back to Home</button></a>
+    '''
 
 if __name__ == '__main__':
     app.run()
-
-
