412cb834700270c6c991d856b41bd14f7112258b
Olliek123 <ollyking123@live.co.uk>
2025-09-29 16:24:56 +0100

    Update app.py
    back to basics

--- app.py
+++ app.py
@@ -1,40 +1,13 @@
-from flask import Flask, render_template_string
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template_string('''
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Home</title>
-        </head>
-        <body>
-            <h1>Hello and welcome to my website!</h1>
-            <form action="/        <button type="submit">Go to About Page</button>
-            </form>
-        </body>
-        </html>
-    ''')
+    return "Hello from Azure Web App!"
 
 @app.route('/about')
-def about():
-    return render_template_string('''
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>About</title>
-        </head>
-        <body>
-            <h1>About Page</h1>
-            <p>This is a second page with some extra info.</p>
-            /
-                <button type="submit">Back to Home</button>
-            </form>
-        </body>
-        </html>
-    ''')
+def home():
+    return "test"
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run()
