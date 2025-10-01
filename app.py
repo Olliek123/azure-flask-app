ffd6e2c660d69348eafe910609be1a58b6090882
Olliek123 <ollyking123@live.co.uk>
2025-10-01 10:40:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,3 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello from Azure Web App!"
-
-@app.route('/about')
-def about():
-    return "Welcome to the about page"
-
-@app.route('/index')
-def third():
-    return "Welcome to the index page
